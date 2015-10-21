150a93ce53e8e317d07a72e1448914d7390317cc
User <user@Users-Mac.local>
2015-10-21 11:50:16 +0300

    Initial Commit

---
+++ ContactsListStart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContactsListStart
+//
+//  Created by User on 10/21/15.
+//  Copyright (c) 2015 Harman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ContactsListStart/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  ContactsListStart
+//
+//  Created by User on 10/21/15.
+//  Copyright (c) 2015 Harman. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
